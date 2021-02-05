2dcb7607fc2910b41d35740ed07570dc89d605c2
Ismoil Hasanov <ismail@pop-os.localdomain>
2021-02-05 18:40:14 +0200

    Version 2.0.1

--- server/server.js
+++ server/server.js
@@ -152,14 +152,14 @@ app.use('/charge', paymentRoute);
 app.use('/paid', paidRoute);
 
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    });
 
-// }
+}
 
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
 
 
 const run = async () => {
