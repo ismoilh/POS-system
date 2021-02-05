b76724d8f361fe565660f56292b6824f9f959e5f
Ismoil Hasanov <ismail@pop-os.localdomain>
2021-02-05 23:13:33 +0200

    Version 2.0.5

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -26,7 +26,7 @@ module.exports.register = async (req, res, next) => {
             subject: 'Account Activation Link',
             html: `
                 <h2>Please verify your account by given link</h2>
-                <p>https://glacial-falls-22941.herokuapp.com/auth/activate/${token}</p>
+                <p>https://pacific-shelf-87157.herokuapp.com/auth/activate/${token}</p>
             `
         };
         mg.messages().send(data, function (err, body) {
