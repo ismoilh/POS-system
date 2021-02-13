0aa863f04a3f0212aefd8c9b4c5e13030e06422c
Ismoil Hasanov <ismail@pop-os.localdomain>
2021-02-14 01:19:58 +0200

    Version 3.1.1

--- server/models/category.js
+++ server/models/category.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
 
 const nameSchema = new Schema({
-    name: String
+    name: String,
+    images: [ImageSchema]
 })
 
 module.exports = mongoose.model('Category', nameSchema);
