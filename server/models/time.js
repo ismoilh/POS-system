eaea49ff42a90a89f1444292785d2058a4a41caf
Ismoil Hasanov <ismail@pop-os.localdomain>
2021-03-09 04:02:44 +0200

    Version 2.7.2

--- server/models/time.js
+++ server/models/time.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 
 const timeSchema = new Schema({
-    ClosingTime: { type: Date, default: Date },
-    OpeningTime: { type: Date, default: Date },
-    KendimOpen: { type: Date, default: Date },
-    KendimClose: { type: Date, default: Date }
+    ClosingTime: Number,
+    OpeningTime: Number,
+    KendimOpen: Number,
+    KendimClose: Number
 })
 
 module.exports = mongoose.model('Time', timeSchema);
